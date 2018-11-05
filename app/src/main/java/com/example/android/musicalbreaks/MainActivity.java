3d8a6242f6a735300e3ecd4e7a5651ee7f0ca1d3
olliebrownlow <44783188+olliebrownlow@users.noreply.github.com>
2018-11-05 18:50:24 +0000

    optimised imports

--- app/src/main/java/com/example/android/musicalbreaks/MainActivity.java
+++ app/src/main/java/com/example/android/musicalbreaks/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.android.musicalbreaks;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
